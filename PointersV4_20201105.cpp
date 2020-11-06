ece97d18650fddff8c6986afcbb309eb4404c308
Ales Bratanic <alijumc@hotmail.com>
2020-11-06 09:29:15 +0100

    Accessing the elemnts in array

--- PointersV4_20201105.cpp
+++ PointersV4_20201105.cpp
@@ -5,8 +5,30 @@
 
 int main()
 {
-	int b[3][2][2] = { 2,3,6,4,5,8 };
-	
+	int c1[3][2][2] = { 1,2,3,4,5,6,7,8,9,10,11,12 }; //starting address hypoteticaly 800
+	int c2[3][2][2] = { {{2,3},{3,4}},
+						{{5,6},{7,8}},
+						{{9,10},{11,12}} };
+	int(*p)[2][2] = c1;
+	printf("The address of first element is %p\n", c1);	//800
+	printf("The address of the first element in array is %p\n", *p);	//800
+	printf("The value of the first element in array is %d\n", *(*p[0]));	//1
+	printf("The value of the first element in the second array is %d\n", *(*p[1]));	//5
+	printf("The addres of the first element in the second array is %p\n", *p[1]);	//816
+	printf("The value of the first element in the third array is %d\n", *(*p[2]));	//9
+	printf("The value of the second element in array is %d\n", *(p[0][0]+1));	//2,	+ is pointer arithmetic
+	printf("The value of the third element in array is %d\n", *(*(p[0]+1)));	//3
+	printf("The value of the fourth element in array is %d\n", *(*(p[0] + 1)+1));	//4
+	printf("The value of the seventh element in array is %d\n", *(*(*(p+ 1) + 1)));	//7
+	printf("The value of the eith element in array is %d\n", *(*(*(p + 1) + 1)+1));	//8
+	//*(p+1) pointer to the second element in first array +1 pointer arithmetic to the second element in the second array
+	//+1 pointer arithmetic to the second element in the third array
+	//*p[0] is pointer to the integer and then we are executing only pointer arithmetic
+	printf("The value of the first element in the first array is %d\n", *(*(*(p))));	//1
+	printf("The value of the first element in the second array is %d\n", *(*(*(p+1))));	//5
+	printf("The value of the first element in the second array is %d\n", *(*p[1]));	//5
+	printf("The value of the second element in the second array is %d\n", *(*p[1]+1));	//6
+	printf("The value of the first element in the third array is %d\n", *(*p[2]));	//9
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
